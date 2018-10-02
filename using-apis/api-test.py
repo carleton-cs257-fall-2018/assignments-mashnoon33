4c58dcc58cc173212e08a759ccb37752d0135489
Mashnoon <mashny@res-237-152.dyn.carleton.edu>
2018-10-01 20:07:08 -0500

    Refactored and met the specs

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -11,66 +11,57 @@ import json
 import requests
 import sys
 
+class Apitest:
+    def __init__(self,type, param):
+        self.api_url_base = "http://www.omdbapi.com/?apikey=515febc6&"
 
-def get_ssh_keys(query, string):
-    api_token = '340187c3b6aa4d838984bcff8594c47d'
-    api_url_base = "http://www.omdbapi.com/?apikey=515febc6&"
-
-    response = requests.get(api_url_base, params={"s": query} )
-    jsonify = json.loads(response.text)
-
-    for item in jsonify['Search'] :
-        response_ = requests.get(api_url_base, params={"i": item['imdbID']})
-        jsonify_ = json.loads(response_.text)
-        organize(jsonify_, string)
+        if type=="search":
+            self.search(param)
+        if type=="movie":
+            self.getMovieByName(param)
+        if type=="id":
+            self.getMovieByID(param)
 
 
+    def getMovieByName(self, param):
+        response = requests.get(self.api_url_base, params={"t": param})
+        jsonify = json.loads(response.text)
+        try:
+            self._printer(jsonify)
+        except KeyError:
+            print('\n No Movies found')
 
-        # print(item['display_title'], item['mpaa_rating'], item['opening_date'])
 
-#
-    # response = requests.get(api_url, headers=headers)
+    def getMovieByID(self, param):
+        response = requests.get(self.api_url_base, params={"i": param})
+        jsonify = json.loads(response.text)
+        try:
+            self._printer(jsonify)
+        except KeyError:
+            print('\n No Movies found')
 
-def organize(movieDict, string):
-    if string == "none":
-        print(movieDict['Title'] + " : " + movieDict['Year'] + " - " + movieDict['Rated'])
-    if string == "date":
-        print(movieDict['Title'] + " : " + movieDict['Date'])
-    if string == "time":
-        print(movieDict['Title'] + " : " + movieDict['Time'])
-    if string == "awards":
-        print(movieDict['Title'] + " : " + movieDict['Awards'])
-    if string == "language":
-        print(movieDict['Title'] + " : " + movieDict['Language'])
-    if string == "director":
-        print(movieDict['Title'] + " : " + movieDict['Director'])
-    if string == "writer":
-        print(movieDict['Title'] + " : " + movieDict['Writer'])
-    if string == "actors":
-        print(movieDict['Title'] + " : " + movieDict['Actors'])
-    if string == "plot":
-        print(movieDict['Title'] + " : " + movieDict['Plot'])
-    if string == "ratings":
-        ratingsArray = movieDict['Ratings']
-        for item in ratingsArray:
-            print(item['Source', ":", item['Value']])
-    else:
-        raise ValueError("This is not a possible second argument")
-    
+    def search(self,param):
+        response = requests.get(self.api_url_base, params={"s": param} )
+        jsonify = json.loads(response.text)
+        try:
+            for item in jsonify['Search'] :
+                self._printer(item)
+        except KeyError:
+            print('\n No Movies found')
+
+    def _printer(self,movieDict):
+        dict = movieDict
+        for key in dict:
+            if key not in ["Response", "Poster","imdbVotes", "imdbID", "Website"] and type(dict[key])==type(""):
+                print(key , ":" , dict[key])
+        print("\n")
+
 def main():
-    if len(sys.argv) >= 2:
-        print('Searching for "' + sys.argv[1] + '"')
-        print()
-        if len(sys.argv) == 2:
-            get_ssh_keys(sys.argv[1],"none")
-        if len(sys.argv) < 3:
-            print("Too many arguments")
-        elif len(sys.argv[2]) == 3:
-            print("test" + " " + sys.argv[2])
-            get_ssh_keys(sys.argv[1],sys.argv[2])
+    if len(sys.argv) >1:
+        run = Apitest(sys.argv[1],sys.argv[2])
     else:
-        print("Enter a title to search the API for it e.g. | api-test.py moviename date/time/awards/language/director/writer/actors/plot/ratings")
-
+        print("Usage statement")
+#NOTE : FIX THIS
 
 
 if __name__ == '__main__':
