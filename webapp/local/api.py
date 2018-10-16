faaf7b0d90bfb543882df81400f6d6c546b753ce
mashNY <mashnoon33@gmail.com>
2018-10-16 18:32:38 -0500

    Minor chnage to doc

---
+++ webapp/local/api.py
@@ -0,0 +1,11 @@
+#DUMMY STUBS
+
+class api:
+    def __init__(self):
+        pass
+    def name(self, name):
+        return []
+    def param(self, *, SAT_AVG=None, ADM_RATE=None, MN_EARN_WNE_INC3_P10=None, type=None):
+        return []
+    def school(self, id):
+        return {}
