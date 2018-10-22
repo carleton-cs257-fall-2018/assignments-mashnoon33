64def5f4f163b3be08520839132eb906fe86dade
mashNY <mashnoon33@gmail.com>
2018-10-21 22:51:14 -0500

    moved some files around

--- webapp/link.js
+++
@@ -1,8 +0,0 @@
-const encodeGetParams = p =>
-  Object.entries(p).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
-
-const params = {
-  
-};
-
-console.log("http://localhost:8080/schools/?" + encodeGetParams(params))
